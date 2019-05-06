a590f0d5640617874256ec26c151976360eec64f
tiagowl <40669081+tiagowl@users.noreply.github.com>
2019-05-05 21:26:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+document.getElementById('formulario').addEventListener('submit', cadastraVeiculo);
+
+function cadastraVeiculo(e){
+
+    var modeloCarro = document.getElementById('modeloCarro').value;
+    var placaCarro = document.getElementById('placaCarro').value;
+    var time = new Date();
+
+    carro = {
+        modelo: modeloCarro,
+        placa: placaCarro,
+        hora: time.getHours(),
+        minutos: time.getMinutes()
+    }
+
+    if(localStorage.getItem('patio2') === null ){
+        var carros = [];
+        carros.push(carro);
+        localStorage.setItem('patio2', JSON.stringify(carros));
+    }else{
+        var carros = JSON.parse(localStorage.getItem('patio2'));
+        carros.push(carro);
+        localStorage.setItem('patio2', JSON.stringify(carros));
+    }
+
+
+
+    e.preventDefault();
+}
+
+function mostrarPatio(){
+
+    var carros = JSON.parse(localStorage.getItem('patio2'));
+
+    var carrosResultado = document.getElementById('resultados');
+
+    carrosResultado.innerHTML = '';
+
+    for(var i=0; i < carros.length; i++){
+        var modelo = carros[i].modelo;
+        var placa = carros[i].placa;
+        var hora = carros[i].hora;
+        var minutos = carros[i].minutos;
+
+         carrosResultado.innerHTML += '<tr>'+
+                               '<td>' + modelo +'</td>' +
+                               '<td>' + placa + '</td>' +
+                               '<td>' + hora + '</td>' +
+                               '<td>' + minutos + '</td>' +
+                               '</tr>';
+    }
+
+}
---
+++ todos.js
@@ -0,0 +1,36 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer café',
+    'Estudar javascript',
+    'Acessar comunidade da Rocket Seat'
+];
+
+function renderTodos(){
+
+    listElement.innerHTML = '';
+
+    for(todo of todos){
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
+function addTodo(){
+    var todoText = inputElement.value;
+    todos.push(todoText);
+    inputElement.value = "";
+    renderTodos();    
+}
+
+buttonElement.onClick = addTodo;
+
+
+
